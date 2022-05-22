2a8e7693690969db4dae3ac525c3a8d025acac8e
Antoine <98873193+Antoine-Mtx@users.noreply.github.com>
2022-05-22 22:25:28 +0200

    defined the index method

--- src/Controller/DomaineController.php
+++ src/Controller/DomaineController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DomaineController extends AbstractController
 {
     /**
-     * @Route("/domaine", name="app_domaine")
+     * @Route("/domaine", name="index_domaine")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response// on injecte le service Doctrine dans la méthode index de notre controller, ce qui nous permettra d'interagir avec notre base de données
     {
+        $domaines = $doctrine->getRepository(Domaine::class)->findAll(); // on désigne le repository de la classe "Domaine" à notre gestionnaire $doctrine puis on utilise la méthode findAll() pour récupérer toutes les instances de cette classe
+
         return $this->render('domaine/index.html.twig', [
-            'controller_name' => 'DomaineController',
+            'domaines' => $domaines,
         ]);
     }
+
+    
 }
