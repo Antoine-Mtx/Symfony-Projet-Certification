94f7524f3c1ee90c4f993f635d47920d02cafa64
Antoine <antoine.mortureux@gmail.com>
2022-05-26 18:17:00 +0200

    added type templates

---
+++ migrations/Version20220523141805.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220523141805 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commentaire (id INT AUTO_INCREMENT NOT NULL, auteur_id INT NOT NULL, competence_id INT NOT NULL, commentaire LONGTEXT NOT NULL, INDEX IDX_67F068BC60BB6FE6 (auteur_id), INDEX IDX_67F068BC15761DAB (competence_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE competence (id INT AUTO_INCREMENT NOT NULL, concepteur_id INT NOT NULL, domaine_id INT NOT NULL, intitule VARCHAR(50) NOT NULL, description LONGTEXT NOT NULL, date_creation DATE NOT NULL, image VARCHAR(255) DEFAULT NULL, icone VARCHAR(255) DEFAULT NULL, INDEX IDX_94D4687F4D472506 (concepteur_id), INDEX IDX_94D4687F4272FC9F (domaine_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE composant (id INT AUTO_INCREMENT NOT NULL, type_id INT NOT NULL, competence_id INT NOT NULL, concepteur_id INT NOT NULL, intitule VARCHAR(50) NOT NULL, contenu JSON NOT NULL, INDEX IDX_EC8486C9C54C8C93 (type_id), INDEX IDX_EC8486C915761DAB (competence_id), INDEX IDX_EC8486C94D472506 (concepteur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE domaine (id INT AUTO_INCREMENT NOT NULL, intitule VARCHAR(50) NOT NULL, description LONGTEXT DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, icone VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, expediteur_id INT NOT NULL, destinataire_id INT NOT NULL, contenu LONGTEXT NOT NULL, date_envoi DATETIME NOT NULL, INDEX IDX_B6BD307F10335F61 (expediteur_id), INDEX IDX_B6BD307FA4F84F6E (destinataire_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, intitule VARCHAR(50) NOT NULL, description LONGTEXT NOT NULL, couleur VARCHAR(10) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(50) NOT NULL, date_creation DATE NOT NULL, UNIQUE INDEX UNIQ_8D93D64986CC499D (pseudo), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC60BB6FE6 FOREIGN KEY (auteur_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC15761DAB FOREIGN KEY (competence_id) REFERENCES competence (id)');
+        $this->addSql('ALTER TABLE competence ADD CONSTRAINT FK_94D4687F4D472506 FOREIGN KEY (concepteur_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE competence ADD CONSTRAINT FK_94D4687F4272FC9F FOREIGN KEY (domaine_id) REFERENCES domaine (id)');
+        $this->addSql('ALTER TABLE composant ADD CONSTRAINT FK_EC8486C9C54C8C93 FOREIGN KEY (type_id) REFERENCES type (id)');
+        $this->addSql('ALTER TABLE composant ADD CONSTRAINT FK_EC8486C915761DAB FOREIGN KEY (competence_id) REFERENCES competence (id)');
+        $this->addSql('ALTER TABLE composant ADD CONSTRAINT FK_EC8486C94D472506 FOREIGN KEY (concepteur_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F10335F61 FOREIGN KEY (expediteur_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FA4F84F6E FOREIGN KEY (destinataire_id) REFERENCES `user` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC15761DAB');
+        $this->addSql('ALTER TABLE composant DROP FOREIGN KEY FK_EC8486C915761DAB');
+        $this->addSql('ALTER TABLE competence DROP FOREIGN KEY FK_94D4687F4272FC9F');
+        $this->addSql('ALTER TABLE composant DROP FOREIGN KEY FK_EC8486C9C54C8C93');
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC60BB6FE6');
+        $this->addSql('ALTER TABLE competence DROP FOREIGN KEY FK_94D4687F4D472506');
+        $this->addSql('ALTER TABLE composant DROP FOREIGN KEY FK_EC8486C94D472506');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F10335F61');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FA4F84F6E');
+        $this->addSql('DROP TABLE commentaire');
+        $this->addSql('DROP TABLE competence');
+        $this->addSql('DROP TABLE composant');
+        $this->addSql('DROP TABLE domaine');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
