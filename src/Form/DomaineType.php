588426b820ebe79a4d338222e294553eb2cb5da6
Antoine <antoine.mortureux@gmail.com>
2022-06-04 16:41:05 +0200

    various changes

--- src/Form/DomaineType.php
+++ src/Form/DomaineType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class DomaineType extends AbstractType
 {
@@ -15,7 +16,7 @@ class DomaineType extends AbstractType
     {
         $builder
         ->add('intitule', TextType::class)
-        ->add('description', TextType::class)
+        ->add('description', TextareaType::class)
         ->add('Valider', SubmitType::class)
         ;
     }
