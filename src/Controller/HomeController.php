d4cd45c832751b1cb805b74233f75f693f63014a
Antoine <antoine.mortureux@gmail.com>
2022-06-05 22:58:29 +0200

    added a data filter to home view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Domaine;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $domaines = $doctrine->getRepository(Domaine::class)->findAll(); // on désigne le repository de la classe "Domaine" à notre gestionnaire $doctrine puis on utilise la méthode findAll() pour récupérer toutes les instances de cette classe
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'domaines' => $domaines,
         ]);
     }
 }
