8125b78f25789e8839979c35001ddc63d1c651c2
Antoine <antoine.mortureux@gmail.com>
2022-07-03 23:36:45 +0200

    edited the portal view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     /**
+     * @Route("/portal", name="app_portal")
+     */
+    public function portal(ManagerRegistry $doctrine): Response
+    {
+        $domaines = $doctrine->getRepository(Domaine::class)->findAll(); // on désigne le repository de la classe "Domaine" à notre gestionnaire $doctrine puis on utilise la méthode findAll() pour récupérer toutes les instances de cette classe
+        $utilisateurs = $doctrine->getRepository(User::class)->findAll();
+        $competences = $doctrine->getRepository(Competence::class)->findAll();
+
+        $nbConcepteurs = 0;
+        foreach ($utilisateurs as $index => $utilisateur) {
+            if (in_array("ROLE_CREATOR", $utilisateur->getRoles())) {
+                $nbConcepteurs ++;
+            }
+        }
+
+        return $this->render('home/portal.html.twig', [
+            'domaines' => $domaines,
+            'utilisateurs' => $utilisateurs,
+            'competences' => $competences,
+            'nbConcepteurs' => $nbConcepteurs,
+        ]);
+    }
+
+    /**
      * @Route("/home", name="app_home")
      */
     public function index(ManagerRegistry $doctrine): Response
